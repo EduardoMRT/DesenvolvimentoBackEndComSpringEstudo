234aef7e7a3fcc1cefdc60f1d9cd092a822c0d0d
EduardoMRT <eduksoficial@gmail.com>
2024-02-02 14:16:03 -0300

    Anotações da Entidade

--- drogaria2/src/main/java/br/com/eduardo/drogaria2/domain/Produto.java
+++ drogaria2/src/main/java/br/com/eduardo/drogaria2/domain/Produto.java
@@ -3,6 +3,11 @@ package br.com.eduardo.drogaria2.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,15 +15,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity //diz que essa classe 'produto' é uma entidade
 public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Short codigo;
 	
+	@Column(length = 50, nullable = false)
 	private String nome;
 	
+	@Column(nullable = false) // unique false = pode repetir
 	private Short quantidade;
 	
+	@Column(nullable = false, precision = 5, scale = 2)
 	private BigDecimal preco;
 
+	@Column(nullable = false)
 	private LocalDate dataDeValidade;
 	
 	
