c6f7a0548322c1e23c2c92f531cd95ce716c01fc
EduardoMRT <eduksoficial@gmail.com>
2024-02-02 11:10:03 -0300

    inicialiazação da config spring

---
+++ drogaria2/src/main/java/br/com/eduardo/drogaria2/Drogaria2Application.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.drogaria2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Drogaria2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Drogaria2Application.class, args);
+	}
+
+}
---
+++ drogaria2/src/test/java/br/com/eduardo/drogaria2/Drogaria2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.drogaria2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Drogaria2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
